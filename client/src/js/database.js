77af3c4c35e8fa369e7911ec3e362d9854e331ef
Hiro Protagonist <shd327@gmail.com>
2023-01-08 14:23:24 -0700

    converted most of putDB code but need to look into the store.put() part

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,16 +17,16 @@ export const putDb = async (content) => {
   console.log('Post to the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const request = store.put({});
 
   // Get confirmation of the request.
   const result = await request;
