c4b752c15354c59350dee15f48d88e6432ade580
Hiro Protagonist <shd327@gmail.com>
2023-01-09 17:08:30 -0700

    finished db.js get and put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({});
+  const request = store.put({ id: 1, tx: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -41,7 +41,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
@@ -52,7 +52,13 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  if (result.value !== null) {
+    return result.value
+  }
+  else {
+    console.log("error with result")
+    return;
+  }
 };
 
 initdb();
